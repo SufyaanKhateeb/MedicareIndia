80ae421c3e92362519648408a66308ae40168e51
Sufyaan Khateeb <sufyaankhateeb@gmail.com>
2021-09-05 19:43:52 +0530

    update

--- medicare/src/App.js
+++ medicare/src/App.js
@@ -1,12 +1,15 @@
-
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+  useEffect(() => {
+    fetch("https://medicareind-api.herokuapp.com/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return <div className="App">Hello</div>;
 }
 
 export default App;
