602771d719e523ab8629bb6baf7de3e2c3cd8ea2
03pawan <pawankumar03012002@gmail.com>
2021-09-06 23:35:45 +0530

    Card back

--- medicare/src/components/DataCard.jsx
+++ medicare/src/components/DataCard.jsx
@@ -2,30 +2,62 @@ import React,{useState} from "react";
 import { useSelector } from 'react-redux';
 
 const DataCard = ({ hospital }) => {
+  const [flip,setFlip] = useState(true);
   return (
+    
     <div className="dataCardcontainer d-inline">
-      <div className="card" style={{ width: "18rem", minHeight: "250px" }}>
-        <div
-          className="card-body"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: "flex-start"
-          }}
-        >
-          <h5 className="card-title">{hospital[2]}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{hospital[4]}</h6>
-          <span className="card-city">{hospital[1]}, {hospital[0]}</span>
-          <span className="card-text"></span>
-          <span>
-            <h4 className="card-rating">{hospital[12]}/10</h4>
-          </span>
-          <button className="btn btn-primary">
-            More
-          </button>
+      {flip ? (<div>
+        <div className="card" style={{ width: "18rem", minHeight: "250px" }}>
+          <div
+            className="card-body"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "flex-start"
+            }}
+          >
+            <h5 className="card-title">{hospital[2]}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{hospital[4]}</h6>
+            <span className="card-city">{hospital[1]}, {hospital[0]}</span>
+            <span className="card-text"></span>
+            <span>
+              <h4 className="card-rating">{hospital[12]}/10</h4>
+            </span>
+            <button className="btn btn-primary" onClick={() => {
+              setFlip(!flip);
+            }} >
+              More
+            </button>
+          </div>
         </div>
-      </div>
+      </div>):(<div>
+          <div className="card" style={{ width: "18rem", minHeight: "250px" }}>
+            <div
+              className="card-body"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignItems: "flex-start"
+              }}
+            >
+              <h5 className="card-title">Address :</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{hospital[5]} <span>{hospital[6]}</span></h6>
+              <span className="card-city">{hospital[7]}</span>
+              
+              <span>
+                <h5 className="card-rating"><a target="_blank" href= {`https://${hospital[8]}`} rel="noreferrer">Visit Us</a></h5>
+              </span>
+              <button className="btn btn-primary" onClick={() => {
+                setFlip(!flip);
+              }} >
+                Less
+              </button>
+            </div>
+          </div>
+      </div>)}
+      
     </div>
   );
 };
