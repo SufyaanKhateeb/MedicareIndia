1fb6d121aa4d28f40418100ed7f6b363fbaa0250
03pawan <pawankumar03012002@gmail.com>
2021-09-06 11:45:29 +0530

    Location

--- medicare/src/components/pages/Location.jsx
+++ medicare/src/components/pages/Location.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 const Location = () => {
+
+    const [location, setLocation] = useState("");
     return (
         <div className="location">
             <div className="search-bar">
-                <div class="input-group">
-                    <div class="form-outline">
-                        <input type="search" id="form1" class="form-control" placeholder="Enter location"/>
-                        <label class="form-label" for="form1">Search</label>
+                <div className="input-group">
+                    <div className="form-outline">
+                        <input type="search" id="form1"  className="form-control"  placeholder="Enter location" onChange={(e)=>{
+                            setLocation(e.target.value);    
+                            }}  />
+                        <label className="form-label" htmlFor="form1">Search</label>
                     </div>
-                    <button type="button" class="btn btn-primary">
-                        <i class="bi bi-search"></i>
+                    <button type="button" className="btn btn-primary" onClick={()=>{document.querySelector('#form1').value="";}}>
+                        <i className="bi bi-search"></i>
                     </button>
                 </div>
+            </div>
+            <div className="Render container">
+
             </div>
         </div>
     )
